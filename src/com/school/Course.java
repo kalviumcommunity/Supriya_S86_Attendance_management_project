d2a959cb3ba5a710a04a1c7af2fbae2b6e06edcc
unknown <nallakukkala.supriya.s86@kalvium.community>
2025-09-22 10:36:16 +0530

    Part 04

--- src/com/school/Course.java
+++ src/com/school/Course.java
@@ -1,16 +1,24 @@
 package com.school;
 
 public class Course {
+    private static int nextCourseIdCounter = 101;
     private int courseId;
     private String courseName;
 
-    public void setDetails(int courseId, String courseName) {
-        this.courseId = courseId;
+    public Course(String courseName) {
+        this.courseId = nextCourseIdCounter++;
         this.courseName = courseName;
     }
 
     public void displayDetails() {
-        System.out.println("Course ID: " + this.courseId);
+        System.out.println("Course ID: C" + this.courseId);
         System.out.println("Course Name: " + this.courseName);
+    }
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
     }
 }
